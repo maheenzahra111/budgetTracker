ed0cf4c2727ee287b9a3948632112c7a6293a516
maheenzahra111 <maheenzahra111@gmail.com>
2023-08-24 17:20:24 +0500

    First commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,65 +1,83 @@
-// src/components/Dashboard.js
-import React, { useState } from 'react';
-import { Button, Table, Input, DatePicker, Menu, Modal, Form, Dropdown } from 'antd';
-import { EditOutlined, DeleteOutlined, PlusOutlined, MoreOutlined } from '@ant-design/icons';
-import DateFilter from './DateFilter';
+import React, { useState, useEffect } from 'react';
+import { Button, Table, Input, DatePicker, Menu, Modal, Form, Dropdown, Row } from 'antd';
+import { EditOutlined, DeleteOutlined, MoreOutlined } from '@ant-design/icons';
 import '../styles/Dashboard.css';
-import { Card, Col, Row } from 'antd';
-import Meta from 'antd/es/card/Meta';
+import dayjs from 'dayjs';
+// import customParseFormat from 'dayjs/plugin/customParseFormat';
+// import advancedFormat from 'dayjs/plugin/advancedFormat';
+// import localizedFormat from 'dayjs/plugin/localizedFormat';
+
+import 'dayjs/locale/zh-cn'; // Import the desired locale
+// dayjs.extend(customParseFormat);
+// dayjs.extend(advancedFormat);
+// dayjs.extend(localizedFormat);
 
 
 const Dashboard = () => {
     const [dataSource, setDataSource] = useState([]);
-    const [searchText, setSearchText] = useState('');
     const [isModalVisible, setIsModalVisible] = useState(false);
+    const [editingData, setEditingData] = useState(null);
     const [form] = Form.useForm();
+    const [filterDate, setFilterDate] = useState(null);
 
-    const pageSize = 10; // Number of entries to show per page
+    const [value, setValue] = useState('')
 
-    const handleFilter = (selectedDate) => {
-        // Implement your logic to filter entries based on the selected date
-        console.log('Filtering by:', selectedDate);
-    };
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-    const handleSearch = () => {
-        // Implement your search logic here
-        console.log('Searching for:', searchText);
+    const fetchData = () => {
+        // Simulate fetching data
+        const data = [
+            // Sample data
+            { key: 1, name: 'Item 1', price: 100, date: '2023-08-24' },
+            { key: 2, name: 'Item 2', price: 200, date: '2023-08-25' },
+            // ... (other data entries)
+        ];
+        setDataSource(data);
     };
 
     const handleEdit = (record) => {
-        form.setFieldsValue(record);
         setIsModalVisible(true);
+        setEditingData(record);
+        form.setFieldsValue(record);
     };
 
     const handleDelete = (recordKey) => {
         Modal.confirm({
-            title: 'Are you sure, you want to delete this record?',
+            title: 'Are you sure you want to delete this record?',
             okText: 'Yes',
-            okType: "danger",
+            okType: 'danger',
             onOk: () => {
-                setDataSource(pre => {
-                    return pre.filter((item) => item.key !== recordKey);
-                })
-            }
-        })
+                setDataSource((prev) => prev.filter((item) => item.key !== recordKey));
+            },
+        });
     };
-
 
     const handleModalOk = () => {
         form.validateFields()
             .then((values) => {
                 const newData = [...dataSource];
-
-                // Add a new entry
-                newData.push({
-                    ...values,
-                    date: new Date(values.date.format('YYYY-MM-DD')),
-                    key: 1,
-                });
-                console.log(newData);
+                if (editingData) {
+                    const index = newData.findIndex((item) => item.key === editingData.key);
+                    if (index > -1) {
+                        newData[index] = {
+                            ...editingData,
+                            ...values,
+                            date: dayjs(values.date).format('YYYY-MM-DD'), // Format the date properly
+                        };
+                    }
+                } else {
+                    newData.push({
+                        ...values,
+                        date: dayjs(values.date).format('YYYY-MM-DD'), // Format the date properly
+                        key: newData.length + 1,
+                    });
+                }
                 setDataSource(newData);
                 form.resetFields();
                 setIsModalVisible(false);
+                setEditingData(null);
             })
             .catch((errorInfo) => {
                 console.log('Validation Failed:', errorInfo);
@@ -67,7 +85,14 @@ const Dashboard = () => {
     };
 
 
-
+    const handleFilter = () => {
+        if (filterDate) {
+            const filtered = dataSource.filter((data) => {
+                return dayjs(data.date).isSame(filterDate, 'day');
+            });
+            setDataSource(filtered);
+        }
+    };
     const columns = [
         {
             title: 'Name',
@@ -83,9 +108,11 @@ const Dashboard = () => {
             title: 'Date',
             dataIndex: 'date',
             key: 'date',
-            render: (date) => <span>{date.toLocaleDateString("en-US")}</span>,
+            render: (date) => {
+                const formattedDate = dayjs(date).format('YYYY-MM-DD');
+                return <span>{formattedDate}</span>;
+            },
         },
-
         {
             title: 'Actions',
             key: 'actions',
@@ -96,13 +123,14 @@ const Dashboard = () => {
             ),
         },
     ];
-
+    
+    
     const menu = (record) => (
         <Menu>
             <Menu.Item key="1" icon={<EditOutlined />} onClick={() => handleEdit(record)}>
                 Edit
             </Menu.Item>
-            <Menu.Item style={{ color: "red" }} key="2" icon={<DeleteOutlined />} onClick={() => handleDelete(record.key)}>
+            <Menu.Item style={{ color: 'red' }} key="2" icon={<DeleteOutlined />} onClick={() => handleDelete(record.key)}>
                 Delete
             </Menu.Item>
         </Menu>
@@ -112,28 +140,37 @@ const Dashboard = () => {
         <div className="dashboard-container">
             <div className="dashboard-header">
                 <Row className="button-row">
-                    <Col>
-                        <DateFilter onFilter={handleFilter} />
-                    </Col>
-                    <Col>
+                    <div>
+                        <DatePicker onChange={(date) => setFilterDate(date)} />
+                        <Button className="filter-button" type="primary" onClick={handleFilter}>
+                            Filter Records
+                        </Button>
+                    </div>
+                    <div>
                         <Button className="add-button" type="primary" onClick={() => setIsModalVisible(true)}>
                             Add Budget
                         </Button>
-                    </Col>
+                    </div>
                 </Row>
             </div>
             <Table columns={columns} dataSource={dataSource} pagination={false} />
-            {/* Add Budget Modal */}
+            {/* Add/Edit Budget Modal */}
             <Modal
-                title="Add Budget"
+                title={editingData ? 'Edit Budget' : 'Add Budget'}
                 visible={isModalVisible}
                 onOk={handleModalOk}
-                onCancel={() => setIsModalVisible(false)}
+                onCancel={() => {
+                    setIsModalVisible(false);
+                    setEditingData(null);
+                    form.resetFields();
+                }}
             >
                 <Form form={form}>
-                    <Form.Item name="key" hidden>
-                        <Input />
-                    </Form.Item>
+                    {editingData && (
+                        <Form.Item name="key" hidden>
+                            <Input />
+                        </Form.Item>
+                    )}
                     <Form.Item
                         name="name"
                         label="Name"
@@ -153,7 +190,7 @@ const Dashboard = () => {
                         label="Date"
                         rules={[{ required: true, message: 'Please input the date' }]}
                     >
-                        <DatePicker />
+                        <DatePicker  />
                     </Form.Item>
                 </Form>
             </Modal>
