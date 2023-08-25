58c0ae3b8b5d3dfa70e68f4087c8c128757a49d8
maheenzahra111 <maheenzahra111@gmail.com>
2023-08-25 11:07:15 +0500

    Third commit

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,201 +1,148 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Table, Input, DatePicker, Menu, Modal, Form, Dropdown, Row } from 'antd';
-import { EditOutlined, DeleteOutlined, MoreOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import {
+  Button,
+  Modal,
+  Table,
+  Form,
+  Input,
+  DatePicker,
+  Space,
+  Row,
+} from 'antd';
+import moment from 'moment';
 import '../styles/Dashboard.css';
-import dayjs from 'dayjs';
-// import customParseFormat from 'dayjs/plugin/customParseFormat';
-// import advancedFormat from 'dayjs/plugin/advancedFormat';
-// import localizedFormat from 'dayjs/plugin/localizedFormat';
-
-import 'dayjs/locale/zh-cn'; // Import the desired locale
-// dayjs.extend(customParseFormat);
-// dayjs.extend(advancedFormat);
-// dayjs.extend(localizedFormat);
-
+import '../styles/dateFilter.css';
 
 const Dashboard = () => {
-    const [dataSource, setDataSource] = useState([]);
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [editingData, setEditingData] = useState(null);
-    const [form] = Form.useForm();
-    const [filterDate, setFilterDate] = useState(null);
-
-    const [value, setValue] = useState('')
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    const fetchData = () => {
-        // Simulate fetching data
-        const data = [
-            // Sample data
-            { key: 1, name: 'Item 1', price: 100, date: '2023-08-24' },
-            { key: 2, name: 'Item 2', price: 200, date: '2023-08-25' },
-            // ... (other data entries)
-        ];
-        setDataSource(data);
-    };
-
-    const handleEdit = (record) => {
-        setIsModalVisible(true);
-        setEditingData(record);
-        form.setFieldsValue(record);
-    };
-
-    const handleDelete = (recordKey) => {
-        Modal.confirm({
-            title: 'Are you sure you want to delete this record?',
-            okText: 'Yes',
-            okType: 'danger',
-            onOk: () => {
-                setDataSource((prev) => prev.filter((item) => item.key !== recordKey));
-            },
-        });
-    };
-
-    const handleModalOk = () => {
-        form.validateFields()
-            .then((values) => {
-                const newData = [...dataSource];
-                if (editingData) {
-                    const index = newData.findIndex((item) => item.key === editingData.key);
-                    if (index > -1) {
-                        newData[index] = {
-                            ...editingData,
-                            ...values,
-                            date: dayjs(values.date).format('YYYY-MM-DD'), // Format the date properly
-                        };
-                    }
-                } else {
-                    newData.push({
-                        ...values,
-                        date: dayjs(values.date).format('YYYY-MM-DD'), // Format the date properly
-                        key: newData.length + 1,
-                    });
-                }
-                setDataSource(newData);
-                form.resetFields();
-                setIsModalVisible(false);
-                setEditingData(null);
-            })
-            .catch((errorInfo) => {
-                console.log('Validation Failed:', errorInfo);
-            });
-    };
-
-
-    const handleFilter = () => {
-        if (filterDate) {
-            const filtered = dataSource.filter((data) => {
-                return dayjs(data.date).isSame(filterDate, 'day');
-            });
-            setDataSource(filtered);
-        }
-    };
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: 'Price',
-            dataIndex: 'price',
-            key: 'price',
-        },
-        {
-            title: 'Date',
-            dataIndex: 'date',
-            key: 'date',
-            render: (date) => {
-                const formattedDate = dayjs(date).format('YYYY-MM-DD');
-                return <span>{formattedDate}</span>;
-            },
-        },
-        {
-            title: 'Actions',
-            key: 'actions',
-            render: (_, record) => (
-                <Dropdown overlay={menu(record)}>
-                    <MoreOutlined />
-                </Dropdown>
+  const [entries, setEntries] = useState([]);
+  const [editingEntry, setEditingEntry] = useState(null);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [form] = Form.useForm();
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+  const handleOk = () => {
+    form
+      .validateFields()
+      .then((values) => {
+        if (editingEntry) {
+          const updatedEntry = {
+            id: editingEntry.id,
+            date: values.date.format('YYYY-MM-DD'),
+            transactionName: values.transactionName,
+            price: values.price,
+          };
+          setEntries(
+            entries.map((entry) =>
+              entry.id === updatedEntry.id ? updatedEntry : entry,
             ),
-        },
-    ];
-    
-    
-    const menu = (record) => (
-        <Menu>
-            <Menu.Item key="1" icon={<EditOutlined />} onClick={() => handleEdit(record)}>
-                Edit
-            </Menu.Item>
-            <Menu.Item style={{ color: 'red' }} key="2" icon={<DeleteOutlined />} onClick={() => handleDelete(record.key)}>
-                Delete
-            </Menu.Item>
-        </Menu>
-    );
-
-    return (
-        <div className="dashboard-container">
-            <div className="dashboard-header">
-                <Row className="button-row">
-                    <div>
-                        <DatePicker onChange={(date) => setFilterDate(date)} />
-                        <Button className="filter-button" type="primary" onClick={handleFilter}>
-                            Filter Records
-                        </Button>
-                    </div>
-                    <div>
-                        <Button className="add-button" type="primary" onClick={() => setIsModalVisible(true)}>
-                            Add Budget
-                        </Button>
-                    </div>
-                </Row>
-            </div>
-            <Table columns={columns} dataSource={dataSource} pagination={false} />
-            {/* Add/Edit Budget Modal */}
-            <Modal
-                title={editingData ? 'Edit Budget' : 'Add Budget'}
-                visible={isModalVisible}
-                onOk={handleModalOk}
-                onCancel={() => {
-                    setIsModalVisible(false);
-                    setEditingData(null);
-                    form.resetFields();
-                }}
-            >
-                <Form form={form}>
-                    {editingData && (
-                        <Form.Item name="key" hidden>
-                            <Input />
-                        </Form.Item>
-                    )}
-                    <Form.Item
-                        name="name"
-                        label="Name"
-                        rules={[{ required: true, message: 'Please input the name' }]}
-                    >
-                        <Input type="text" />
-                    </Form.Item>
-                    <Form.Item
-                        name="price"
-                        label="Price"
-                        rules={[{ required: true, message: 'Please input the price' }]}
-                    >
-                        <Input type="number" />
-                    </Form.Item>
-                    <Form.Item
-                        name="date"
-                        label="Date"
-                        rules={[{ required: true, message: 'Please input the date' }]}
-                    >
-                        <DatePicker  />
-                    </Form.Item>
-                </Form>
-            </Modal>
+          );
+          setEditingEntry(null);
+        } else {
+          const newEntry = {
+            id: Date.now(),
+            date: values.date.format('YYYY-MM-DD'),
+            transactionName: values.transactionName,
+            price: values.price,
+          };
+          setEntries([...entries, newEntry]);
+        }
+        form.resetFields();
+        setIsModalVisible(false);
+      })
+      .catch((errorInfo) => {
+        console.log('Validation failed:', errorInfo);
+      });
+  };
+  const handleCancel = () => {
+    setEditingEntry(null);
+    form.resetFields();
+    setIsModalVisible(false);
+  };
+  const handleEdit = (id) => {
+    const entryToEdit = entries.find((entry) => entry.id === id);
+    if (entryToEdit) {
+      form.setFieldsValue({
+        date: moment(entryToEdit.date),
+        transactionName: entryToEdit.transactionName,
+        price: entryToEdit.price,
+      });
+      setEditingEntry(entryToEdit);
+      showModal();
+    }
+  };
+  const handleDelete = (id) => {
+    setEntries(entries.filter((entry) => entry.id !== id));
+  };
+  const columns = [
+    {
+      title: 'Date/Time',
+      dataIndex: 'date',
+      key: 'date',
+    },
+    {
+      title: 'Transaction Name',
+      dataIndex: 'transactionName',
+      key: 'transactionName',
+    },
+    {
+      title: 'Price',
+      dataIndex: 'price',
+      key: 'price',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <div>
+          <Button onClick={() => handleEdit(record.id)}>Edit</Button>
+          <Button
+            onClick={() => handleDelete(record.id)}
+            style={{ marginLeft: '8px' }}
+          >
+            Delete
+          </Button>
         </div>
-    );
+      ),
+    },
+  ];
+  return (
+    <div className="dashboard-container">
+      <Row className="button-row">
+        <Space>
+          <DatePicker />
+          <Button className="filter-button" type="primary">
+            Filter Records
+          </Button>
+        </Space>
+        <Button className="add-button" type="primary" onClick={showModal}>
+          Add Budget
+        </Button>
+      </Row>
+      <Modal
+        title={editingEntry ? 'Edit Budget Entry' : 'Add New Budget Entry'}
+        open={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Form form={form} layout="vertical">
+          <Form.Item name="date" label="Date/Time" rules={[{ required: true }]}>
+            <DatePicker format="YYYY-MM-DD" />
+          </Form.Item>
+          <Form.Item
+            name="transactionName"
+            label="Transaction Name"
+            rules={[{ required: true }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="price" label="Price" rules={[{ required: true }]}>
+            <Input type="number" />
+          </Form.Item>
+        </Form>
+      </Modal>
+      <Table dataSource={entries} columns={columns} />
+    </div>
+  );
 };
-
 export default Dashboard;
